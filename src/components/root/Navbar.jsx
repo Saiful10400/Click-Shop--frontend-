911a68fca1a9d1c103e5d3acd5e3a0dd36237871
saiful10400 <saifulislam10400i5@gmail.com>
2023-10-21 17:32:35 +0600

    footer have changed

--- src/components/root/Navbar.jsx
+++ src/components/root/Navbar.jsx
@@ -20,7 +20,7 @@ const{darkHandle,dark}=useContext(parentContext)
   }
   const li=(
     <>
-    <li><NavLink to={"/"} className="btn lg:mx-2 leading-loose">Home</NavLink></li>
+    <li className=""><NavLink to={"/"} className="btn lg:mx-2 leading-loose">Home</NavLink></li>
     <li><NavLink to={"/addProduct"} className="btn lg:mx-2 leading-loose">Add product</NavLink></li>
     <li className=""><NavLink to={"/myCart"} className="btn lg:mx-2 leading-loose">My Cart</NavLink></li>
 
@@ -34,7 +34,7 @@ const{darkHandle,dark}=useContext(parentContext)
       <label tabIndex={0} className="btn btn-ghost lg:hidden">
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
       </label>
-      <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+      <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-gray-700 rounded-box w-52">
        
             {li}
             
